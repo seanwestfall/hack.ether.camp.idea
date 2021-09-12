ede52749c8bdf12c4ac8ec020f188b609f82c899
seanwestfall <sean.westfall@gmail.com>
2021-09-12 12:48:04 -0700

    update the plugin description

--- plugin/src/plugin.c
+++ plugin/src/plugin.c
@@ -2,6 +2,20 @@
 #include <ctype.h>
 #include <mysql/plugin.h>
 
-mysql_declare_plugin("ETH_interaction")
-
+mysql_declare_plugin(ETH_interaction)
+{
+  MYSQL_FTPARSER_PLUGIN,      /* type                            */
+  &simple_parser_descriptor,  /* descriptor                      */
+  "simple_parser",            /* name                            */
+  "Oracle Corporation",       /* author                          */
+  "Simple Full-Text Parser",  /* description                     */
+  PLUGIN_LICENSE_GPL,         /* plugin license                  */
+  simple_parser_plugin_init,  /* init function (when loaded)     */
+  simple_parser_plugin_deinit,/* deinit function (when unloaded) */
+  0x0001,                     /* version                         */
+  simple_status,              /* status variables                */
+  simple_system_variables,    /* system variables                */
+  NULL,
+  0
+}
 mysql_declare_plugin_end;
